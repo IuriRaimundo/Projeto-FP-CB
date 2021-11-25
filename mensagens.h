dc83079237a79b1576e8885df29cdf0c34791871
iuraimundo <iuraimundo@deloitte.pt>
2021-11-25 11:06:16 +0000

    atualização de mensagens

--- mensagens.h
+++ mensagens.h
@@ -4,6 +4,6 @@
 // Mensagens de erro/sucesso
 #define MENSAGEM_ERRO_GRAVAR "\nErro ao gravar ficheiro.\n"
 #define MENSAGEM_SUCESSO_GRAVAR "\nDados gravados com sucesso.\n"
-#define MENSAGEM_ERRO_LER "\nErro ao ler ficheiros.\n"
+#define MENSAGEM_ERRO_LER "\nNao foram lidos os ficheiros.\n"
 
 #endif // MENSAGENS_H_INCLUDED
